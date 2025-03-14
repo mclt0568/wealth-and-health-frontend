13cf3740097756a5eacfff776821066af879c988
Frankium <tis7bfrankie@gmail.com>
2025-03-15 04:09:07 +1100

    styles

--- lib/styles.dart
+++ lib/styles.dart
@@ -1,10 +1,30 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 class AppStyles {
-  static const Color accentBackground = Color(0xFFC2F2EA);
-  static const Color accentForeground = Color(0xFF14594C);
+  static const Color accentBackground = Color.fromARGB(255, 194, 242, 213);
+  static const Color accentForeground = Color.fromARGB(255, 40, 119, 62);
   static const Color primaryBackground = Color(0xFFFFFFFF);
   static const Color secondaryBackground = Color(0xFFF3F3F3);
   static const Color primaryForeground = Color(0xFF393939);
-  static const Color secondaryForeground = Color.fromARGB(255, 102, 102, 102);
+  static const Color secondaryForeground = Color(0xFF666666);
+
+  static const BoxShadow shadow = BoxShadow(
+    blurRadius: 5,
+    // spreadRadius: -3,
+    offset: Offset(0, 3),
+    color: Color(0x77C5C5C5),
+  );
+
+  static const TextStyle title = TextStyle(
+    fontSize: 45,
+    fontWeight: FontWeight.w300,
+    color: AppStyles.primaryForeground,
+  );
+
+  static const TextStyle paragraph = TextStyle(
+    fontSize: 20,
+    color: AppStyles.primaryForeground,
+  );
 }
